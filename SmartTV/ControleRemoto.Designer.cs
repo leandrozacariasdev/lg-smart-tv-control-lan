bffc85ac455d4201b6e92b60806aa1203fb22856
leandroventron <leandro.zacarias@ventron.com.br>
2015-04-28 20:49:34 -0300

    Files.

---
+++ SmartTV/ControleRemoto.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace SmartTV
+{
+    partial class ControleRemoto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.btnParear = new System.Windows.Forms.Button();
+            this.txtParingKey = new System.Windows.Forms.TextBox();
+            this.btnRequestParing = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Location = new System.Drawing.Point(12, 89);
+            this.txtResponse.Multiline = true;
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.Size = new System.Drawing.Size(252, 156);
+            this.txtResponse.TabIndex = 1;
+            // 
+            // btnParear
+            // 
+            this.btnParear.Location = new System.Drawing.Point(189, 34);
+            this.btnParear.Name = "btnParear";
+            this.btnParear.Size = new System.Drawing.Size(75, 23);
+            this.btnParear.TabIndex = 2;
+            this.btnParear.Text = "Parear";
+            this.btnParear.UseVisualStyleBackColor = true;
+            this.btnParear.Click += new System.EventHandler(this.btnParear_Click);
+            // 
+            // txtParingKey
+            // 
+            this.txtParingKey.Location = new System.Drawing.Point(47, 34);
+            this.txtParingKey.Name = "txtParingKey";
+            this.txtParingKey.Size = new System.Drawing.Size(136, 20);
+            this.txtParingKey.TabIndex = 3;
+            // 
+            // btnRequestParing
+            // 
+            this.btnRequestParing.Location = new System.Drawing.Point(270, 6);
+            this.btnRequestParing.Name = "btnRequestParing";
+            this.btnRequestParing.Size = new System.Drawing.Size(164, 23);
+            this.btnRequestParing.TabIndex = 4;
+            this.btnRequestParing.Text = "Requisitar Key";
+            this.btnRequestParing.UseVisualStyleBackColor = true;
+            this.btnRequestParing.Click += new System.EventHandler(this.btnRequestParing_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(277, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(47, 31);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(277, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(47, 31);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "_";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(333, 97);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(47, 35);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "▲";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(333, 180);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(47, 36);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "▼";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(277, 138);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(47, 36);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "◄";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(387, 137);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(47, 36);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "►";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(333, 138);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(47, 36);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "OK";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(278, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Volume";
+            this.label1.Click += new System.EventHandler(this.Volume);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(277, 180);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(47, 36);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "Menu";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(333, 60);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(47, 31);
+            this.button9.TabIndex = 14;
+            this.button9.Text = "Exit";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(387, 179);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(47, 37);
+            this.button10.TabIndex = 15;
+            this.button10.Text = "Back";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(393, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Canal";
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(386, 97);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(48, 35);
+            this.button12.TabIndex = 18;
+            this.button12.Text = "_";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(386, 60);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(48, 31);
+            this.button13.TabIndex = 17;
+            this.button13.Text = "+";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(277, 222);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(157, 23);
+            this.button14.TabIndex = 20;
+            this.button14.Text = "Desligar";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(39, 8);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(225, 20);
+            this.txtIP.TabIndex = 21;
+            this.txtIP.Text = "192.168.131.137";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "IP:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Key:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Resposta:";
+            // 
+            // ControleRemoto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 259);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnRequestParing);
+            this.Controls.Add(this.txtParingKey);
+            this.Controls.Add(this.btnParear);
+            this.Controls.Add(this.txtResponse);
+            this.MaximumSize = new System.Drawing.Size(477, 297);
+            this.MinimumSize = new System.Drawing.Size(477, 297);
+            this.Name = "ControleRemoto";
+            this.Text = "LG Controle Remoto";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtResponse;
+        private System.Windows.Forms.Button btnParear;
+        private System.Windows.Forms.TextBox txtParingKey;
+        private System.Windows.Forms.Button btnRequestParing;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ SmartTV/ControleRemoto.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartTV
+{
+    public partial class ControleRemoto : Form
+    {
+        public ControleRemoto()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRequestParing_Click(object sender, EventArgs e)
+        {
+            Requisitar("<auth><type>AuthKeyReq</type></auth>", true);
+        }
+
+        private void btnParear_Click(object sender, EventArgs e)
+        {
+            Requisitar(string.Format("<auth><type>AuthReq</type><value>{0}</value></auth>", txtParingKey.Text), true);
+        }
+
+        public int Requisitar(string request, bool auth = false)
+        {
+            try
+            {
+                IPAddress host = IPAddress.Parse(txtIP.Text);
+                IPEndPoint hostep = new IPEndPoint(host, 8080);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+                socket.Connect(hostep);
+                StringBuilder sb = new StringBuilder();
+
+                if (auth)
+                    sb.AppendLine("POST /roap/api/auth HTTP/1.1");
+                else
+                    sb.AppendLine("POST /roap/api/command HTTP/1.1");
+
+                sb.AppendLine("Host: 192.168.131.137");
+                sb.AppendLine("Content-type: application/atom+xml");
+                sb.AppendLine("Content-length: " + request.Length);
+                sb.AppendLine("Connection: close");
+                sb.AppendLine("");
+                sb.AppendLine(request);
+
+                var byteCount = socket.Send(Encoding.ASCII.GetBytes(sb.ToString()));
+                if (byteCount > 0)
+                {
+                    byte[] bytes = new byte[byteCount];
+                    byteCount = socket.Receive(bytes, SocketFlags.None);
+                    txtResponse.Text = Encoding.UTF8.GetString(bytes);
+                }
+
+                socket.Close();
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("{0} Error code: {1}.", e.Message, e.ErrorCode);
+                return (e.ErrorCode);
+            }
+            return 0;
+        }
+
+        enum CommandEnum
+        {
+            POWER = 1,
+            NUMBER_0 = 2,
+            NUMBER_1 = 3,
+            NUMBER_2 = 4,
+            NUMBER_3 = 5,
+            NUMBER_4 = 6,
+            NUMBER_5 = 7,
+            NUMBER_6 = 8,
+            NUMBER_7 = 9,
+            NUMBER_8 = 10,
+            NUMBER_9 = 11,
+            UP = 12,
+            DOWN = 13,
+            LEFT = 14,
+            RIGHT = 15,
+            OK = 20,
+            HOME_MENU = 21,
+            BACK = 23,
+            VOLUME_UP = 24,
+            VOLUME_DOWN = 25,
+            MUTE_TOGGLE = 26,
+            CHANNEL_UP = 27,
+            CHANNEL_DOWN = 28,
+            BLUE = 29,
+            GREEN = 30,
+            RED = 31,
+            YELLOW = 32,
+            PLAY = 33,
+            PAUSE = 34,
+            STOP = 35,
+            FAST_FORWARD = 36,
+            REWIND = 37,
+            SKIP_FORWARD = 38,
+            SKIP_BACKWARD = 39,
+            RECORD = 40,
+            RECORDING_LIST = 41,
+            REPEAT = 42,
+            LIVE_TV = 43,
+            EPG = 44,
+            PROGRAM_INFORMATION = 45,
+            ASPECT_RATIO = 46,
+            EXTERNAL_INPUT = 47,
+            PIP_SECONDARY_VIDEO = 48,
+            SHOW_SUBTITLE = 49,
+            PROGRAM_LIST = 50,
+            TELE_TEXT = 51,
+            MARK = 52,
+            VIDEO_3D = 400,
+            LR_3D = 401,
+            DASH = 402,
+            PREVIOUS_CHANNEL = 403,
+            FAVORITE_CHANNEL = 404,
+            QUICK_MENU = 405,
+            TEXT_OPTION = 406,
+            AUDIO_DESCRIPTION = 407,
+            ENERGY_SAVING = 409,
+            AV_MODE = 410,
+            SIMPLINK = 411,
+            EXIT = 412,
+            RESERVATION_PROGRAM_LIST = 413,
+            PIP_CHANNEL_UP = 414,
+            PIP_CHANNEL_DOWN = 415,
+            SWITCH_VIDEO = 416,
+            APPS = 417
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            SendCommand(CommandEnum.VOLUME_UP);
+
+        }
+
+        private void SendCommand(CommandEnum cmd)
+        {
+            Requisitar(string.Format("<command><name>HandleKeyInput</name><value>{0}</value></command>", (int)cmd));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SendCommand(CommandEnum.VOLUME_DOWN);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SendCommand(CommandEnum.UP);
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SendCommand(CommandEnum.RIGHT);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SendCommand(CommandEnum.LEFT);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SendCommand(CommandEnum.DOWN);
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SendCommand(CommandEnum.OK);
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            SendCommand(CommandEnum.UP);
+
+        }
+
+        private int i;
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+            {
+
+            switch ((int)keyData)
+            {
+                case (int)Keys.Up:
+                    SendCommand(CommandEnum.UP);
+                    break;
+                case (int)Keys.Down:
+                    SendCommand(CommandEnum.DOWN);
+                    break;
+                case (int)Keys.Right:
+                    SendCommand(CommandEnum.RIGHT);
+                    break;
+                case (int)Keys.Left:
+                    SendCommand(CommandEnum.LEFT);
+                    break;
+                case (int)Keys.Enter:
+                    SendCommand(CommandEnum.OK);
+                    break;
+                case 109:
+                    SendCommand(CommandEnum.VOLUME_DOWN);
+                    break;
+                case 107:
+                    SendCommand(CommandEnum.VOLUME_UP);
+                    break;
+            }
+
+            Thread.Sleep(80);
+            // Call the base class
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void Volume(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            SendCommand(CommandEnum.HOME_MENU);
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            SendCommand(CommandEnum.EXIT);
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            SendCommand(CommandEnum.BACK);
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            SendCommand(CommandEnum.LR_3D);
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            SendCommand(CommandEnum.CHANNEL_UP);
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            SendCommand(CommandEnum.CHANNEL_DOWN);
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            SendCommand(CommandEnum.POWER);
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            SendCommand(CommandEnum.PROGRAM_LIST);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+    }
+}
